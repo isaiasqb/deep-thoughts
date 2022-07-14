098eff23f5366b02941ab1b77490b20b68eeada8
IsaiasQB <isaias.qb@hotmail.com>
2022-07-14 01:13:37 -0400

    add reactions functionality

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -6,10 +6,11 @@ import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 //import the reactions component
 import ReactionList from '../components/ReactionList';
+import Auth from '../utils/auth'
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const {id: thoughtId } = useParams();
-  console.log(thoughtId);
 
   //The variables loading and data are destructured from the useQuery Hook. 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
@@ -37,6 +38,7 @@ const SingleThought = props => {
       </div>
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
 
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id}/>}
     </div>
   );
 };
