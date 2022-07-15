9c26a26abfc272dd6c4c042d09a419b8b1db9927
IsaiasQB <isaias.qb@hotmail.com>
2022-07-15 00:55:24 -0400

    changes to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,8 +5,8 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
